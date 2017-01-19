d543d6c84babbb8984756ffaceba410fa494c8d4
ohataken <ohataken@outlook.com>
2017-01-19 21:09:59 +0900

    Initial Commit

---
+++ lopez/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  lopez
+//
+//  Created by Ken Ohata on 2017/01/19.
+//  Copyright © 2017年 Ken Ohata. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ lopez/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  lopez
+//
+//  Created by Ken Ohata on 2017/01/19.
+//  Copyright © 2017年 Ken Ohata. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
