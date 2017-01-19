95ba3005a003cc0b8237c4696aaba660771b9fa6
ohataken <ohataken@outlook.com>
2017-01-19 21:11:17 +0900

    Add a new target "lopez-safari"

---
+++ lopez-safari/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  lopez-safari
+//
+//  Created by Ken Ohata on 2017/01/19.
+//  Copyright © 2017年 Ken Ohata. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
