1707d64a3558bfe2e3f011a16797b0cc0c14908f
ohataken <ohataken@outlook.com>
2017-01-20 01:54:57 +0900

    Update injection script

--- lopez-safari/script.js
+++ lopez-safari/script.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-});
+const plaintextLink = function(title, url) {
+    return `${title} ${url}`;
+};
+
+const markdownLink = function(title, url) {
+    return `[${title}](${url})`;
+};
+
+const orgmodeLink = function(title, url)  {
+    return `[[${url}][${title}]]`;
+};
+
+const onContextMenu = function(event) {
+    const title = document.title;
+    const url = window.location.href;
+    alert(markdownLink(title, url));
+}
+
+document.addEventListener("contextmenu", onContextMenu, false);
